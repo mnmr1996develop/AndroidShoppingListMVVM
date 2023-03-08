99826c7c1aba0affcf7d344d3ca653f9f54159d1
mnmr1996develop <93550664+mnmr1996develop@users.noreply.github.com>
2023-03-08 17:08:16 -0500

    adding a view holder

---
+++ app/src/main/java/com/michaelrichards/androidshoppinglist/View/ItemViewHolder.java
@@ -0,0 +1,35 @@
+package com.michaelrichards.androidshoppinglist.View;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.michaelrichards.androidshoppinglist.R;
+
+public class ItemViewHolder extends RecyclerView.ViewHolder {
+
+    private TextView mTextView;
+
+    private ItemViewHolder(@NonNull View itemView) {
+        super(itemView);
+        mTextView = itemView.findViewById(R.id.textShoppingListItem);
+    }
+
+    public TextView getTextView() {
+        return mTextView;
+    }
+
+    public void bind(String text){
+        mTextView.setText(text);
+    }
+
+    public static ItemViewHolder create(ViewGroup parent){
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.rv_item, parent, false);
+        return new ItemViewHolder(view);
+    }
+
+}
